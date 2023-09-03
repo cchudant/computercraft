5d865fcba9781ab88676c4d2544310add99b8bfe
cchudant <charlesluc.chudant@orange.fr>
2023-09-03 18:10:10 +0200

    mine2 fix

--- apis/controlApi.lua
+++ apis/controlApi.lua
@@ -352,7 +352,7 @@ function connectControl(sourceid)
 	local turtle = { id = sourceid }
 	for _,method in ipairs(turtleFunctions) do
 		turtle[method] = function(...)
-			local ret = _sendRoundtrip(clientid, 'turtle', {
+			local ret = _sendRoundtrip(sourceid, 'turtle', {
 				method = method,
 				args = {...},
 			})
