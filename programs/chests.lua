f339f3af81921d3938ad7b4a99fe64912138fdfd
cchudant <charlesluc.chudant@orange.fr>
2023-09-06 17:42:02 +0200

    mine2 fix

--- programs/chests.lua
+++ programs/chests.lua
@@ -82,6 +82,7 @@ function push()
 		local itemsPushed = 0
 		for periph, inv in pairs(fullInv) do
 			for i, el in pairs(inv) do
+				print(el.name, retEl.name)
 				if el.name == retEl.name then
 					local stackLimit = retrieve_.getItemLimit(retI)
 					local toPush = stackLimit - math.max(el.count + retEl.count, stackLimit)
