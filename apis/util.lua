cfb03bff7474f5ae4c11fd952f3b0117b3cab88e
cchudant <charlesluc.chudant@orange.fr>
2023-09-07 13:53:46 +0200

    ui begin

---
+++ apis/util.lua
@@ -0,0 +1,257 @@
+function arrayConcat(...)
+    local newTable = {}
+    for _,arr in ipairs({...}) do
+        for _,el in ipairs(arr) do
+            table.insert(newTable, el)
+        end
+    end
+    return newTable
+end
+
+function arrayEvery(arr, func)
+    for i,el in ipairs(arr) do
+        if not func(el, i, arr) then
+            return false
+        end
+    end
+    return true
+end
+
+function arrayAny(arr, func)
+    for i,el in ipairs(arr) do
+        if func(el, i, arr) then
+            return true
+        end
+    end
+    return false
+end
+
+function arrayFilter(arr, func)
+    local newTable = {}
+    for i,el in ipairs(arr) do
+        if func(el, i, arr) then
+            table.insert(newTable, el)
+        end
+    end
+    return newTable
+end
+
+function arrayFind(arr, func)
+    for i,el in ipairs(arr) do
+        if func(el, i, arr) then
+            return el
+        end
+    end
+    return nil
+end
+
+function arrayFindIndex(arr, func)
+    for i,el in ipairs(arr) do
+        if func(el, i, arr) then
+            return i
+        end
+    end
+    return 0
+end
+
+function arrayFindLast(arr, func)
+    for i = table.getn(arr), 1, -1 do
+        local el = arr[i]
+        if func(el, i, arr) then
+            return el
+        end
+    end
+    return nil
+end
+
+function arrayFindLastIndex(arr, func)
+    for i = table.getn(arr), 1, -1 do
+        local el = arr[i]
+        if func(el, i, arr) then
+            return i
+        end
+    end
+    return 0
+end
+
+function arrayFlat(arr, depth)
+    function arrFlat(newTable, arr, depth)
+        if type(arr) == 'table' and (depth == nil or depth > 0) then
+            for i,el in ipairs(arr) do
+                if not arrFlat(newTable, arr, depth - 1) then
+                    table.insert(newTable, el)
+                end
+            end
+            return true
+        else
+            return false
+        end
+    end
+    local newTable = {}
+    arrFlat(newTable, arr, depth)
+    return newTable
+end
+
+function arrayFlatMap(arr, func, depth)
+    function arrFlat(newTable, arr, depth)
+        if type(arr) == 'table' and (depth == nil or depth > 0) then
+            for i,el in ipairs(arr) do
+                local elem = func(el)
+                if not arrFlat(newTable, arr, depth - 1) then
+                    table.insert(newTable, elem)
+                end
+            end
+            return true
+        else
+            return false
+        end
+    end
+    local newTable = {}
+    arrFlat(newTable, arr, depth)
+    return newTable
+end
+
+function arrayForEach(arr, func)
+    for i,el in ipairs(arr) do
+        func(el, i, arr)
+    end
+    return arr
+end
+
+function arrayContains(arr, elem)
+    for i,el in ipairs(arr) do
+        if elem == el then
+            return true
+        end
+    end
+    return false
+end
+
+function arrayIndexOf(arr, elem)
+    for i,el in ipairs(arr) do
+        if elem == el then
+            return i
+        end
+    end
+    return 0
+end
+
+function arrayLastIndexOf(arr, elem)
+    for i = table.getn(arr), 1, -1 do
+        local el = arr[i]
+        if elem == el then
+            return i
+        end
+    end
+    return 0
+end
+
+function arrayJoin(arr, separator)
+    if separator == nil then separator = ',' end
+    local res = ''
+    for i,el in ipairs(arr) do
+        if i == 1 then
+            res = tostring(el)
+        else
+            res = res .. separator .. el
+        end
+    end
+    return res
+end
+
+function arrayMap(arr, func)
+    local newTable = {}
+    for i,el in ipairs(arr) do
+        local newEl = func(el)
+        table.push(newTable, newEl)
+    end
+    return newTable
+end
+
+function arrayPop(arr)
+    return table.remove(arr, table.getn(arr))
+end
+
+function arrayPush(arr, el)
+    return table.insert(arr, el)
+end
+
+function arrayLen(arr)
+    return table.getn(arr)
+end
+
+function arrayReduce(arr, func, accumulator)
+    for i,v in ipairs(arr) do
+        accumulator = func(accumulator, v, i, arr)
+    end
+    return accumulator
+end
+
+function arrayReduceRight(arr, func, accumulator)
+    for i = table.getn(arr), 1, -1 do
+        local v = arr[i]
+        accumulator = func(accumulator, v, i, arr)
+    end
+    return accumulator
+end
+
+function arrayReverse(arr)
+    local newTable = {}
+    for i = table.getn(arr), 1, -1 do
+        local v = arr[i]
+        table.insert(newTable, v)
+    end
+    return newTable
+end
+
+function arrayShift(arr)
+    return table.remove(arr, 1)
+end
+
+-- does not support negative indexes yet
+function arraySlice(arr, start, end_)
+    local newTable = {}
+    for i = start, end_ do
+        local v = arr[i]
+        table.insert(newTable, v)
+    end
+    return newTable
+end
+
+function arraySort(arr, func)
+    return table.sort(arr, func)
+end
+
+function arrayUnshift(arr, ...)
+    local index = 1
+    for _,v in ipairs({...}) do
+        table.insert(arr, v, index)
+        index = index + 1
+    end
+    return table.getn(arr)
+end
+
+function objectEntries(obj)
+    local table = {}
+    for k,v in pairs(obj) do
+        table.insert(table, { k, v })
+    end
+    return table
+end
+
+function objectFromEntries(entries)
+    local obj = {}
+    for _,v in ipairs(entries) do
+        local k,el = unpack(v)
+        obj[k] = el
+    end
+    return obj
+end
+
+function defaultArgs(options, defaults)
+	for k,v in pairs(defaults) do
+		if options[k] == nil then
+			options[k] = defaults[k]
+		end
+	end
+end
---
+++ programs/ui.lua
@@ -0,0 +1,203 @@
+os.loadAPI("/firmware/apis/util.lua")
+
+UIObject = {
+    transparent = false,
+    marginLeft = 0,
+    marginTop = 0,
+    marginBottom = 0,
+    marginRight = 0,
+}
+function UIObject:new(o)
+    o = o or {}
+    setmetatable(o, self)
+    self.__index = self
+    return o
+end
+
+function UIObject:draw(term, x, y, requestedW, requestedH) end
+function UIObject:getSize(requestedW, requestedH) end
+
+Block = UIObject:new {
+    width = 'fill',
+    height = 'fill',
+    backgroundColor = nil,
+    textColor = nil,
+    paddingLeft = 0,
+    paddingTop = 0,
+    paddingBottom = 0,
+    paddingRight = 0,
+
+    minHeight = nil,
+    minWidth = nil,
+    maxHeight = nil,
+    maxWidth = nil,
+
+    childrenDirection = 'right',
+    centerElements = false
+}
+
+local function blockTiling(self, requestedW, requestedH, func)
+    local function computeTiling(onlyOneLine, start)
+        local posX, posY = (self.paddingLeft or 0), (self.paddingTop or 0)
+
+        local blockWidth = requestedW - (self.paddingLeft or 0) - (self.paddingRight or 0)
+        local blockHeight = requestedH - (self.paddingTop or 0) - (self.paddingBottom or 0)
+        local availableW = blockWidth
+        local availableH = blockHeight
+
+        local maxWidthThisLine = 0
+        local maxHeightThisLine = 0
+        if not onlyOneLine then maxWidthThisLine, maxHeightThisLine = computeTiling(true, 1) end
+
+        for i = start, #self do
+            local child = self[i]
+            local w, h = child:getSize(availableW, availableH)
+
+            posX, posY = posX + (child.marginLeft or 0), posY + (child.marginTop or 0)
+
+            local correctedX, correctedY = posX, posY
+            if self.centerItems then
+                if self.childrenDirection == 'bottom' then
+                    local fatW = maxWidthThisLine - w
+                    correctedX = correctedX + math.floor((fatW / 2) + 0.5) -- pseudo round
+                else
+                    local fatH = maxHeightThisLine - h
+                    correctedY = correctedY + math.floor((fatH / 2) + 0.5) -- pseudo round
+                end
+            end
+
+            if func ~= nil then func(child, correctedX, correctedY, w, h) end
+
+            maxWidthThisLine, maxHeightThisLine = math.max(maxWidthThisLine, w), math.max(maxHeightThisLine, h)
+
+            if self.childrenDirection == 'bottom' then
+                posX = posX + w + (child.marginLeft or 0) + (child.marginRight or 0)
+
+                availableW = availableW - (child.marginLeft or 0) - (child.marginRight or 0) - w
+                if availableW <= 0 then
+                    availableW = blockWidth
+                    posX = self.paddingLeft or 0
+                    posY = posY + maxHeightThisLine + (child.marginTop or 0) + (child.marginBottom or 0)
+                    availableH = availableH - h - (child.marginTop or 0) - (child.marginBottom or 0)
+                    maxHeightThisLine = 0
+                    if not onlyOneLine then maxWidthThisLine, maxHeightThisLine = computeTiling(true, i+1)
+                    else return maxWidthThisLine, maxHeightThisLine end
+                end
+            else -- right
+                posY = posY + w + (child.marginLeft or 0) + (child.marginRight or 0)
+
+                availableH = availableH - (child.marginTop or 0) - (child.marginBottom or 0) - w
+                if availableH <= 0 then
+                    availableH = blockHeight
+                    posY = self.paddingTop or 0
+                    posX = posX + maxWidthThisLine + (child.marginLeft or 0) + (child.marginRight or 0)
+                    availableW = availableW - w - (child.marginLeft or 0) - (child.marginRight or 0)
+                    maxWidthThisLine = 0
+                    if not onlyOneLine then maxWidthThisLine, maxHeightThisLine = computeTiling(true, i+1)
+                    else return maxWidthThisLine, maxHeightThisLine end
+                end
+            end
+        end
+
+        if onlyOneLine then return maxWidthThisLine, maxHeightThisLine end
+
+        posX, posY = posX + (self.paddingRight or 0), posY + (self.paddingBottom or 0)
+
+        return posX, posY
+    end
+
+    return computeTiling(func)
+end
+
+function Block:getSize(requestedW, requestedH)
+    return blockTiling(self, requestedW, requestedH)
+end
+
+function Block:draw(term, x, y, requestedW, requestedH)
+    if self.transparent then return end
+
+    local width, height = self:getSize(requestedW, requestedH)
+    if self.backgroundColor ~= nil then
+        term.setBackgroundColor(self.backgroundColor)
+
+        for i = 1, height do
+            term.setCursorPos(x, y + i)
+            for _ = 1, width do
+                term.write(" ")
+            end
+        end
+    end
+
+    blockTiling(self, requestedW, requestedH, function(child, posX, posY, availableW, availableH)
+        if self.textColor ~= nil then
+            term.setTextColor(self.textColor)
+        end
+        if self.backgroundColor ~= nil then
+            term.setBackgroundColor(self.backgroundColor)
+        end
+        child:draw(x + posX, y + posY, availableW, availableH)
+    end)
+end
+
+local function stringDisplaySize(s)
+    if string.len(s) == 0 then return 0, 0 end
+    local maxHeight, maxWidth = 1, 0
+    local currentWidth = 0
+    for i = 1, string.len(s) do
+        local c = s:sub(i, i)
+
+        currentWidth = currentWidth + 1
+        if c == '\n' then
+            maxWidth = math.max(maxWidth, currentWidth)
+            maxHeight = maxHeight + 1
+            currentWidth = 0
+        end
+    end
+    return maxWidth, maxHeight
+end
+
+Text = UIObject:new {
+    text = nil,
+    backgroundColor = nil,
+    textColor = nil,
+}
+function Text:new(text)
+    Block.new(self, {})
+    self.text = text
+    self.width, self.height = stringDisplaySize(self.text)
+end
+function Text:draw(term, x, y, parentW, parentH)
+    if self.transparent then return end
+    term.setCursorPos(x, y)
+    if self.backgroundColor ~= nil then
+        term.setBackgroundColor(self.backgroundColor)
+    end
+    if self.textColor ~= nil then
+        term.setTextColor(self.textColor)
+    end
+    term.write(self.text)
+end
+function Text:getSize()
+    return self.width, self.height
+end
+
+local interface = Block:new {
+    Block:new {
+        width = 'fill',
+        height = 1,
+        Text:new ("Search:"),
+    },
+    Block:new {
+        paddingTop = 1,
+        backgroundColor = colors.gray,
+        Text:new("-> Hello")
+    },
+    Block:new {
+        paddingTop = 1,
+        backgroundColor = colors.gray,
+        Text:new("-> Hello")
+    },
+    Text:new("-> Hello")
+}
+
+interface:draw(term, 1, 1, term.getSize())
