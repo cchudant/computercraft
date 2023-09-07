13381da6b47557e9314e5f7553dbc08904ec65f0
cchudant <charlesluc.chudant@orange.fr>
2023-09-07 16:25:03 +0200

    continue

--- programs/ui.lua
+++ programs/ui.lua
@@ -144,14 +144,12 @@ function Block:draw(term, x, y, requestedW, requestedH)
     if self.backgroundColor ~= nil then
         term.setBackgroundColor(self.backgroundColor)
 
-        print("ddd", x, y, requestedW, requestedH, width, height)
-
-        -- for i = 1, height do
-        --     term.setCursorPos(x, y + i)
-        --     for _ = 1, width do
-        --         term.write(" ")
-        --     end
-        -- end
+        for i = 1, height do
+            term.setCursorPos(x, y + i)
+            for _ = 1, width do
+                term.write(" ")
+            end
+        end
     end
 
     blockTiling(self, requestedW, requestedH, function(child, posX, posY, availableW, availableH)
