b36578ac32ea1f73e0f33f1a8a66b354a437c16c
cchudant <charlesluc.chudant@orange.fr>
2023-09-07 17:25:29 +0200

    mine2 fix

--- programs/ui.lua
+++ programs/ui.lua
@@ -132,7 +132,7 @@ function Block:draw(term, x, y, requestedW, requestedH)
     end
 
     blockTiling(self, requestedW, requestedH, function(child, posX, posY, availableW, availableH)
-    	print(posX, posY, availableW, availableH)
+    	print("call", posX, posY, availableW, availableH)
         term.setTextColor(term.defaultTextColor)
         term.setBackgroundColor(term.defaultBackgroundColor)
         if self.textColor ~= nil then
