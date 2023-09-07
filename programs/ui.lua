e7522fe5e0d71be068ce42ba0ca0d047b9fab87a
cchudant <charlesluc.chudant@orange.fr>
2023-09-07 15:04:46 +0200

    continue

--- programs/ui.lua
+++ programs/ui.lua
@@ -50,6 +50,7 @@ local function blockTiling(self, requestedW, requestedH, func)
         if not onlyOneLine then maxWidthThisLine, maxHeightThisLine = computeTiling(true, 1) end
 
         for i = start or 1, #self do
+        	print(availableW, availableH)
             local child = self[i]
             local w, h = child:getSize(availableW, availableH)
 
