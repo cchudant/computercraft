b30e8caf8611aacf9b1921c818aa8b1528302618
cchudant <charlesluc.chudant@orange.fr>
2023-09-08 19:33:51 +0200

    mine2 fix

--- programs/ui.lua
+++ programs/ui.lua
@@ -383,6 +383,7 @@ local interface = Block:new {
 	    alignContentX = 'begin',
 	    alignContentY = 'begin',
     	makeBlock(),
+    	makeBlock(),
     },
     -- Block:new {
     -- 	width = '100%',
