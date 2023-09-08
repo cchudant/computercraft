25f7861d5fb9970b0cb3c1a79e801d85bd3b04b8
cchudant <charlesluc.chudant@orange.fr>
2023-09-08 21:40:30 +0200

    mine2 fix

--- programs/ui.lua
+++ programs/ui.lua
@@ -251,11 +251,11 @@ function Block:draw(term, x, y, requestedW, requestedH)
 
         posX = posX + align(self.alignContentX, slackW, iInLine, elemsInLine)
         posY = posY + align(self.alignContentY, slackH, iLine, nLines)
+        print(posX, realW, realH)
         drawChild(self, term, child,
         	posX + child.marginLeft, posY + child.marginTop,
         	realW - child.marginLeft - child.marginRight, realH - child.marginTop - child.marginBottom)
 
-        print(posX, realW, realH)
         posX = posX + realW
 
         return true
