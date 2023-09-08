9e92895e91062fc899fc7908c0b5149766e2b958
cchudant <charlesluc.chudant@orange.fr>
2023-09-08 19:59:20 +0200

    mine2 fix

--- programs/ui.lua
+++ programs/ui.lua
@@ -397,7 +397,7 @@ local interface = Block:new {
     width = '100%',
     height = '100%',
     backgroundColor = colors.yellow,
-    makeLine('begin'),
+    makeLine('end'),
     -- makeLine('end'),
     -- makeLine('center'),
     -- makeLine('space'),
