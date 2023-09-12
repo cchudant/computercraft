482c5010633e3612e6f2d0a20a7f8b38a98fa440
Pierre Chaminade <pierre@chaminade.fr>
2023-09-13 01:42:16 +0200

    asd

--- programs/kelp.lua
+++ programs/kelp.lua
@@ -10,46 +10,51 @@ os.loadAPI("/firmware/apis/mine2.lua")
 -- right = tonumber(right)
 -- height = tonumber(height)
 
-depth = 4
-right = 4
+
+function kelpHarvest(depth, right, height)
+
+    mine2.travelCuboid(turtle, {
+        depth = depth,
+        right = right,
+        height = 1,
+        prepareSameLevel = function(funcs, firstBottom, firstUp) end,
+        runBeforeEveryStep = function(funcs)
+            turtle.dig()
+        end,
+        finish = function() end,
+    })
+
+    for j=1,height do
+        turtle.up()
+    end
+
+    mine2.travelCuboid(turtle, {
+        depth = depth,
+        right = right,
+        height = 1,
+        prepareSameLevel = function(funcs, firstBottom, firstUp)
+        end,
+        runBeforeEveryStep = function(funcs)
+            turtle.suckDown()
+        end,
+        finish = function() end,
+    })
+
+    for j=1,height-1 do
+        turtle.digDown()
+        turtle.down()
+    end
+    turtle.down()
+end
+
+
+depth = 10
+right = 10
 height = 8
 
 turtle.dig()
 turtle.forward()
-mine2.travelCuboid(turtle, {
-	depth = depth,
-	right = right,
-	height = 1,
-    prepareSameLevel = function(funcs, firstBottom, firstUp)
-    end,
-	runBeforeEveryStep = function(funcs)
-		turtle.dig()
-	end,
-    finish = function() end,
-})
-
-for j=1,height do
-    turtle.up()
-end
-
-mine2.travelCuboid(turtle, {
-	depth = depth,
-	right = right,
-	height = 1,
-    prepareSameLevel = function(funcs, firstBottom, firstUp)
-    end,
-	runBeforeEveryStep = function(funcs)
-		turtle.suckDown()
-	end,
-    finish = function() end,
-})
-
-for j=1,height-1 do
-    turtle.digDown()
-    turtle.down()
-end
-turtle.down()
-
+kelpHarvest(depth, right, height)
 turtle.back()
 
 
