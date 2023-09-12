8242b10bc0c42a99c289d8f228edeef996781440
cchudant <charlesluc.chudant@orange.fr>
2023-09-13 01:14:19 +0200

    mine2 fix

--- programs/sugarcanefarm.lua
+++ programs/sugarcanefarm.lua
@@ -14,20 +14,31 @@ depth = 37
 right = 40
 height = 1
 
-mine2.travelCuboid(turtle, {
-	depth = depth,
-	right = right,
-	height = height,
-	runBeforeEveryStep = function(funcs)
-		turtle.dig()
-	end,
-	runAfterEveryStep = function(funcs, bottom, up)
-		local success, detail = turtle.inspectDown()
-		if success and detail.name == 'minecraft:sugar_cane' then
-			turtle.digDown()
-		end
-	end,
-	finish = function()
-		turtle.back()
-	end,
-})
+while true do
+	while select_item('minecraft:sugar_cane') do
+		turtle.dropUp()
+	end
+
+	mine2.travelCuboid(turtle, {
+		depth = depth,
+		right = right,
+		height = height,
+		runBeforeEveryStep = function(funcs)
+			turtle.dig()
+		end,
+		runAfterEveryStep = function(funcs, bottom, up)
+			local success, detail = turtle.inspectDown()
+			if success and detail.name == 'minecraft:sugar_cane' then
+				turtle.digDown()
+			end
+		end,
+		finish = function()
+			turtle.back()
+		end,
+	})
+	while select_item('minecraft:sugar_cane') do
+		turtle.dropUp()
+	end
+
+	os.sleep(60*5)
+end
