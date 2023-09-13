2a16a107c56af417e3247a7a293d00def54f5ab4
Pierre Chaminade <pierre@chaminade.fr>
2023-09-13 16:47:43 +0200

    sdf

---
+++ programs/plateform.lua
@@ -0,0 +1,13 @@
+os.loadAPI("/firmware/apis/mine2.lua")
+
+mine2.travelCuboid(turtle, {
+    depth = 17,
+    right = 17,
+    height = 1,
+    prepareSameLevel = function(funcs, firstBottom, firstUp) end,
+    runBeforeEveryStep = function(funcs)
+        mine2.selectItem(turtle, "minecraft:cobbeled_deepslate")
+        turtle.placeDown()
+    end,
+    finish = function() end,
+})
