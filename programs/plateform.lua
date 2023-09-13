dc9a3fc6bf8266285d56d64873f1cdaf81939e25
Pierre Chaminade <pierre@chaminade.fr>
2023-09-13 16:58:19 +0200

    sd

--- programs/plateform.lua
+++ programs/plateform.lua
@@ -6,7 +6,7 @@ mine2.travelCuboid(turtle, {
     height = 1,
     prepareSameLevel = function(funcs, firstBottom, firstUp) end,
     runBeforeEveryStep = function(funcs)
-        mine2.selectItem(turtle, "minecraft:cobbeled_deepslate")
+        mine2.selectItem(turtle, "minecraft:cobbled_deepslate")
         turtle.placeDown()
     end,
     finish = function() end,
