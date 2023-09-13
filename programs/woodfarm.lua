e32b843c860fa68c7f2dec6646fa31da49f13fbf
cchudant <charlesluc.chudant@orange.fr>
2023-09-13 04:30:03 +0200

    mine2 fix

--- programs/woodfarm.lua
+++ programs/woodfarm.lua
@@ -4,40 +4,50 @@ depth = 15
 right = 3
 
 while true do
-	while mine2.selectItem(turtle, 'minecraft:sugar_cane') do
+	while mine2.selectItem(turtle, 'minecraft:oak_log') do
 		turtle.dropUp()
+	end
+
+	function cutTree() 
+		local success, detail = turtle.inspect()
+		if success and detail.name == 'minecraft:oak_log' then
+			turtle.digDown()
+			local success, detail = turtle.inspectUp()
+			local level = 1
+			while success and detail.name == 'minecraft:oak_log' do
+				turtle.digUp()
+				turtle.up()
+				level = level + 1					
+				success, detail = turtle.inspectUp()
+			end
+
+			for _ = level, 1, -1 do
+				turtle.down()
+			end
+
+			if mine2.selectItem(turtle, 'minecraft:sapling') then
+				turtle.placeDown()
+			end
+			mine2.dropExcessItems(turtle, {'minecraft:sapling'}, 2)
+		end
+
+		turtle.suckDown()
 	end
 
 	mine2.travelCuboid(turtle, {
 		depth = depth,
 		right = right,
 		height = 1,
+		prepareSameLevel = function(funcs)
+			turtle.dig()
+			funcs.forward()
+			cutTree()
+		end,
 		runBeforeEveryStep = function(funcs)
 			turtle.dig()
 		end,
-		runAfterEveryStep = function(funcs, bottom, up)
-			local success, detail = turtle.inspect()
-			if success and detail.name == 'minecraft:oak_log' then
-				turtle.digDown()
-				local success, detail = turtle.inspectUp()
-				local level = 1
-				while success and detail.name == 'minecraft:oak_log' do
-					turtle.digUp()
-					turtle.up()
-					level = level + 1					
-					success, detail = turtle.inspectUp()
-				end
-
-				for _ = level, 1, -1 do
-					turtle.down()
-				end
-
-				if mine2.selectItem(turtle, 'minecraft:sapling') then
-					turtle.placeDown()
-				end
-				mine2.dropExcessItems(turtle, {'minecraft:sapling'}, 2)
-			end
-
+		runAfterEveryStep = function(funcs)
+			cutTree()
 		end,
 	})
 
