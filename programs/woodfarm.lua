9858662665918f56ff368492dc7df72d1bef3674
cchudant <charlesluc.chudant@orange.fr>
2023-09-13 04:23:00 +0200

    mine2 fix

--- programs/woodfarm.lua
+++ programs/woodfarm.lua
@@ -1,20 +1,51 @@
 depth = 15
 right = 3
 
-
-function farm()
-	for d=1,depth
-end
-
 while true do
 	while mine2.selectItem(turtle, 'minecraft:sugar_cane') do
 		turtle.dropUp()
 	end
 
-	farm()
-	while mine2.selectItem(turtle, 'minecraft:sugar_cane') do
+	mine2.travelCuboid(turtle, {
+		depth = depth,
+		right = right,
+		height = height,
+		runBeforeEveryStep = function(funcs)
+			turtle.dig()
+		end,
+		runAfterEveryStep = function(funcs, bottom, up)
+			local success, detail = turtle.inspect()
+			if success and detail.name == 'minecraft:oak_log' then
+				turtle.digDown()
+				local success, detail = turtle.inspectUp()
+				local level = 1
+				while success and detail.name == 'minecraft:oak_log' do
+					turtle.digUp()
+					turtle.up()
+					level = level + 1					
+					success, detail = turtle.inspectUp()
+				end
+
+				for _ = level, 1, -1 do
+					turtle.down()
+				end
+
+				if mine2.selectItem(turtle, 'minecraft:sapling') then
+					turtle.placeDown()
+				end
+				mine2.dropExcessItems(turtle, {'minecraft:sapling'}, 2)
+			end
+
+		end,
+	})
+
+	while mine2.selectItem(turtle, 'minecraft:oak_log') do
 		turtle.dropUp()
 	end
 
+	turtle.select(1)
+
 	os.sleep(60*5)
 end
+
+
