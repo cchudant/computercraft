535af72959274450608093deed659653fb84dd19
cchudant <charlesluc.chudant@orange.fr>
2023-09-14 14:41:38 +0200

    ui for chests2

--- programs/chests2.lua
+++ programs/chests2.lua
@@ -4,33 +4,34 @@ local monitor = peripheral.find('monitor')
 monitor.setTextScale(0.5)
 
 ui.drawLoop(
-    ui.Block:new {
-        width = '100%',
-        height = '100%',
-        ui.Block:new {
-            width = '100%',
-            alignContentX = 'spaceBetween',            
-            ui.Block:new {
-                marginLeft = 2,
-                paddingX = 1,
-                backgroundColor = colors.gray,
-                textColor = colors.white,
-                ui.Text:new { text = "Push" }
-            },
-            ui.Block:new { -- search bar
-                ui.Text:new {
-                    text = "Search:",
-                    backgroundColor = colors.gray,
-                    textColor = colors.white
-                },
-                ui.Block:new {
-                    width = 20,
-                    height = 1,
-                    backgroundColor = colors.lightGray,
-                    textColor = colors.white
-                }
-            }
-        }
-    },
+    -- ui.Block:new {
+    --     width = '100%',
+    --     height = '100%',
+    --     ui.Block:new {
+    --         width = '100%',
+    --         alignContentX = 'spaceBetween',            
+    --         ui.Block:new {
+    --             marginLeft = 2,
+    --             paddingX = 1,
+    --             backgroundColor = colors.gray,
+    --             textColor = colors.white,
+    --             ui.Text:new { text = "Push" }
+    --         },
+    --         ui.Block:new { -- search bar
+    --             ui.Text:new {
+    --                 text = "Search:",
+    --                 backgroundColor = colors.gray,
+    --                 textColor = colors.white
+    --             },
+    --             ui.Block:new {
+    --                 width = 20,
+    --                 height = 1,
+    --                 backgroundColor = colors.lightGray,
+    --                 textColor = colors.white
+    --             }
+    --         }
+    --     }
+    -- },
+    ui.Text { text = "hello" },
     monitor
 )
