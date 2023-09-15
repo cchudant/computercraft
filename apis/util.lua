d5e2a5890062b0e3ce62d773cdd19a173c2e00b4
cchudant <charlesluc.chudant@orange.fr>
2023-09-15 19:10:08 +0200

    parallel group

--- apis/util.lua
+++ apis/util.lua
@@ -2,8 +2,8 @@ local util = {}
 
 function util.arrayConcat(...)
     local newTable = {}
-    for _,arr in ipairs({...}) do
-        for _,el in ipairs(arr) do
+    for _, arr in ipairs({ ... }) do
+        for _, el in ipairs(arr) do
             table.insert(newTable, el)
         end
     end
@@ -11,7 +11,7 @@ function util.arrayConcat(...)
 end
 
 function util.arrayEvery(arr, func)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if not func(el, i, arr) then
             return false
         end
@@ -20,7 +20,7 @@ function util.arrayEvery(arr, func)
 end
 
 function util.arrayAny(arr, func)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if func(el, i, arr) then
             return true
         end
@@ -30,7 +30,7 @@ end
 
 function util.arrayFilter(arr, func)
     local newTable = {}
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if func(el, i, arr) then
             table.insert(newTable, el)
         end
@@ -39,7 +39,7 @@ function util.arrayFilter(arr, func)
 end
 
 function util.arrayFind(arr, func)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if func(el, i, arr) then
             return el
         end
@@ -48,7 +48,7 @@ function util.arrayFind(arr, func)
 end
 
 function util.arrayFindIndex(arr, func)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if func(el, i, arr) then
             return i
         end
@@ -79,7 +79,7 @@ end
 function util.arrayFlat(arr, depth)
     local function arrFlat(newTable, arr, depth)
         if type(arr) == 'table' and (depth == nil or depth > 0) then
-            for i,el in ipairs(arr) do
+            for i, el in ipairs(arr) do
                 if not arrFlat(newTable, arr, depth - 1) then
                     table.insert(newTable, el)
                 end
@@ -97,7 +97,7 @@ end
 function util.arrayFlatMap(arr, func, depth)
     local function arrFlat(newTable, arr, depth)
         if type(arr) == 'table' and (depth == nil or depth > 0) then
-            for i,el in ipairs(arr) do
+            for i, el in ipairs(arr) do
                 local elem = func(el)
                 if not arrFlat(newTable, arr, depth - 1) then
                     table.insert(newTable, elem)
@@ -114,14 +114,14 @@ function util.arrayFlatMap(arr, func, depth)
 end
 
 function util.arrayForEach(arr, func)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         func(el, i, arr)
     end
     return arr
 end
 
 function util.arrayContains(arr, elem)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if elem == el then
             return true
         end
@@ -130,7 +130,7 @@ function util.arrayContains(arr, elem)
 end
 
 function util.arrayIndexOf(arr, elem)
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if elem == el then
             return i
         end
@@ -151,7 +151,7 @@ end
 function util.arrayJoin(arr, separator)
     if separator == nil then separator = ',' end
     local res = ''
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         if i == 1 then
             res = tostring(el)
         else
@@ -163,7 +163,7 @@ end
 
 function util.arrayMap(arr, func)
     local newTable = {}
-    for i,el in ipairs(arr) do
+    for i, el in ipairs(arr) do
         local newEl = func(el)
         table.insert(newTable, newEl)
     end
@@ -183,7 +183,7 @@ function util.arrayLen(arr)
 end
 
 function util.arrayReduce(arr, func, accumulator)
-    for i,v in ipairs(arr) do
+    for i, v in ipairs(arr) do
         accumulator = func(accumulator, v, i, arr)
     end
     return accumulator
@@ -226,7 +226,7 @@ end
 
 function util.arrayUnshift(arr, ...)
     local index = 1
-    for _,v in ipairs({...}) do
+    for _, v in ipairs({ ... }) do
         table.insert(arr, v, index)
         index = index + 1
     end
@@ -235,7 +235,7 @@ end
 
 function util.objectKeys(obj)
     local table = {}
-    for k,_ in pairs(obj) do
+    for k, _ in pairs(obj) do
         table.insert(table, k)
     end
     return table
@@ -243,7 +243,7 @@ end
 
 function util.objectValues(obj)
     local table = {}
-    for _,v in pairs(obj) do
+    for _, v in pairs(obj) do
         table.insert(table, v)
     end
     return table
@@ -251,7 +251,7 @@ end
 
 function util.objectEntries(obj)
     local table = {}
-    for k,v in pairs(obj) do
+    for k, v in pairs(obj) do
         table.insert(table, { k, v })
     end
     return table
@@ -259,8 +259,8 @@ end
 
 function util.objectFromEntries(entries)
     local obj = {}
-    for _,v in ipairs(entries) do
-        local k,el = table.unpack(v)
+    for _, v in ipairs(entries) do
+        local k, el = table.unpack(v)
         obj[k] = el
     end
     return obj
@@ -268,36 +268,36 @@ end
 
 function util.defaultArgs(options, defaults)
     if options == nil then options = {} end
-	for k,_ in pairs(defaults) do
-		if options[k] == nil then
-			options[k] = defaults[k]
-		end
-	end
+    for k, _ in pairs(defaults) do
+        if options[k] == nil then
+            options[k] = defaults[k]
+        end
+    end
     return options
 end
 
 ---Create a new random nonce
 ---@return string
 function util.newNonce()
-	return tostring(math.floor(math.random() * 10000000))
+    return tostring(math.floor(math.random() * 10000000))
 end
 
 function util.parallelGroup(...)
     local coroutineIDCounter = 1
-	local coroutines = {}
-	local filters = {}
+    local coroutines = {}
+    local filters = {}
     local nCoroutines = 0
 
     local nonce = util.newNonce()
 
     local addedCoroutines = {}
 
-	for _, func in ipairs({...}) do
+    for _, func in ipairs({ ... }) do
         local coroutineID = coroutineIDCounter
         coroutineIDCounter = coroutineIDCounter + 1
-		coroutines[coroutineID] = coroutine.create(function()
+        coroutines[coroutineID] = coroutine.create(function()
             local function addCoroutines(...)
-                for _, func in ipairs({...}) do
+                for _, func in ipairs({ ... }) do
                     local coroutineID = coroutineIDCounter
                     coroutineIDCounter = coroutineIDCounter + 1
                     addedCoroutines[coroutineID] = func
@@ -308,44 +308,41 @@ function util.parallelGroup(...)
             os.queueEvent("parallelGroup:end:" .. nonce, coroutineID)
         end)
         nCoroutines = nCoroutines + 1
-	end
+    end
 
-	while nCoroutines > 0 do
+    while nCoroutines > 0 do
         print("Waiting...")
-		local bag = {os.pullEvent()}
-		if bag[1] == "parallelGroup:add:" .. nonce then
+        local bag = { os.pullEvent() }
+        if bag[1] == "parallelGroup:add:" .. nonce then
             local coroutineID = bag[2]
             print("Add", coroutineID)
             local func = addedCoroutines[coroutineID]
             addedCoroutines[coroutineID] = nil
-			coroutines[coroutineID] = coroutine.create(function()
-				func()
+            coroutines[coroutineID] = coroutine.create(function()
+                func()
                 os.queueEvent("parallelGroup:end:" .. nonce, coroutineID)
-			end)
+            end)
             nCoroutines = nCoroutines + 1
         elseif bag[1] == "parallelGroup:end:" .. nonce then
             local coroutineID = bag[2]
             print("End", coroutineID)
             coroutines[coroutineID] = nil
             nCoroutines = nCoroutines - 1
-        else
-            print("Other", bag[1])
-            for k,co in pairs(coroutines) do
-                if filters[k] == nil or filters[k] == bag[1] or bag[1] == 'terminate' then
-                    if coroutine.status(co) ~= 'dead' then
-                        print("Resume", k)
-                        local ok, filter = coroutine.resume(co, table.unpack(bag))
-                        if not ok then
-                            error(filter, 0)
-                        end
-                        filters[k] = filter
+        end
+        print("Other", bag[1])
+        for k, co in pairs(coroutines) do
+            if filters[k] == nil or filters[k] == bag[1] or bag[1] == 'terminate' then
+                if coroutine.status(co) ~= 'dead' then
+                    print("Resume", k)
+                    local ok, filter = coroutine.resume(co, table.unpack(bag))
+                    if not ok then
+                        error(filter, 0)
                     end
+                    filters[k] = filter
                 end
             end
         end
-
-	end
+    end
 end
-
 
 return util
