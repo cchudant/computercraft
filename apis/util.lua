63b30bec1bafe75b9046aa6ae9f5603fbc0cb0ce
cchudant <charlesluc.chudant@orange.fr>
2023-09-18 20:05:17 +0200

    crafting in storage

--- apis/util.lua
+++ apis/util.lua
@@ -169,7 +169,7 @@ if fs then
     -- computercraft
     local pretty = require('cc.pretty').pretty_print
     function util.prettyPrint(...)
-        for _, v in {...} do
+        for _, v in ipairs({...}) do
             pretty(v)
         end
     end
