b950cf64f29bd55b84d247301fc043371a974876
cchudant <charlesluc.chudant@orange.fr>
2023-09-18 20:35:24 +0200

    crafting in storage

--- apis/storage/ui.lua
+++ apis/storage/ui.lua
@@ -6,44 +6,42 @@ local storageUI = {}
 ---@param term Redirect
 ---@param storageConnection any
 ---@return fun() startUI
-function storageUI.create(term, storageConnection)
-    return function()
-        ui.drawLoop(ui.Block:new {
-            width = 10,
-            height = 10,
+function storageUI.runUI(term, storageConnection)
+    ui.drawLoop(ui.Block:new {
+        width = 10,
+        height = 10,
+        ui.Block:new {
+            width = '100%',
+            alignContentX = 'spaceBetween',
             ui.Block:new {
-                width = '100%',
-                alignContentX = 'spaceBetween',
-                ui.Block:new {
-                    marginLeft = 2,
-                    paddingX = 1,
+                marginLeft = 2,
+                paddingX = 1,
+                backgroundColor = colors.gray,
+                textColor = colors.white,
+                onClick = function()
+                    storageConnection.transfer({
+                        type = 'storeItems',
+                        source = 'minecraft:chest_20',
+                        amount = 'all',
+                    })
+                end,
+                ui.Text:new { text = "Push" },
+            },
+            ui.Block:new {     -- search bar
+                ui.Text:new {
+                    text = "Search:",
                     backgroundColor = colors.gray,
-                    textColor = colors.white,
-                    onClick = function()
-                        storageConnection.transfer({
-                            type = 'storeItems',
-                            source = 'minecraft:chest_20',
-                            amount = 'all',
-                        })
-                    end,
-                    ui.Text:new { text = "Push" },
+                    textColor = colors.white
                 },
-                ui.Block:new { -- search bar
-                    ui.Text:new {
-                        text = "Search:",
-                        backgroundColor = colors.gray,
-                        textColor = colors.white
-                    },
-                    ui.TextInput:new {
-                        width = 20,
-                        height = 1,
-                        backgroundColor = colors.lightGray,
-                        textColor = colors.white
-                    }
+                ui.TextInput:new {
+                    width = 20,
+                    height = 1,
+                    backgroundColor = colors.lightGray,
+                    textColor = colors.white
                 }
             }
-        }, term)
-    end
+        }
+    }, term)
 end
 
 return storageUI
--- programs/chests2.lua
+++ programs/chests2.lua
@@ -3,22 +3,22 @@ local storage = require('storage')
 local storageUI = require('storage.ui')
 local util = require('util')
 
-local startStorageServer, storageConnection = storage.storageServer({
+local startStorageServer, makeStorageConnection = storage.storageServer({
     crafters = { inventory = "minecraft:chest_27", computerID = 28 },
     storageChests = { "minecraft:chest_24", "minecraft:chest_23", "minecraft:chest_22" },
     craft = true,
 })
 
-util.prettyPrint("2", storageConnection)
+util.prettyPrint("2", makeStorageConnection)
 
 local monitor = peripheral.find('monitor') --[[@as Monitor]]
 monitor.setTextScale(0.5)
 
 parallel.waitForAny(
     startStorageServer,
-    storageUI.create(monitor, storageConnection()),
+    function() storageUI.runUI(monitor, makeStorageConnection()) end,
     function()
-        local connection = storageConnection()
+        local connection = makeStorageConnection()
         local success, missing, consumed = connection.craftItem("minecraft:dried_kelp_block", 1)
         pretty({success, missing, consumed})
         print('finished')
