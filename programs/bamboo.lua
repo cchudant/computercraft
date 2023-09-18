975a96346c8a6666bcb77202349ee6644bc32690
Pierre Chaminade <pierre@chaminade.fr>
2023-09-18 22:08:44 +0200

    sdf

---
+++ programs/bamboo.lua
@@ -0,0 +1,28 @@
+os.loadAPI("/firmware/apis/mine2.lua")
+
+-- local depth, right, height = ...
+-- if depth == nil or right == nil or height == nil then
+-- 	print("usage: mine2 <depth> <right> <height>")
+-- 	return
+-- end
+
+-- depth = tonumber(depth)
+-- right = tonumber(right)
+-- height = tonumber(height)
+
+function placeDirt(depth, right)
+    mine2.travelCuboid(turtle, {
+        depth = depth,
+        right = right,
+        height = 1,
+        prepareSameLevel = function(funcs, firstBottom, firstUp) end,
+        runBeforeEveryStep = function(funcs)
+            mine2.selectItem("minecraft:dirt")
+            turtle.placeDown()
+        end,
+        finish = function() end,
+    })
+end
+
+
+placeDirt(32, 16)
