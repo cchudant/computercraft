c496c861c5923e8b48adc74b6971b79115c0dee3
Pierre Chaminade <pierre@chaminade.fr>
2023-09-21 13:15:50 +0200

    b

--- programs/langton.lua
+++ programs/langton.lua
@@ -1,19 +1,19 @@
 local mine2 = require("apis.mine2")
 
 function applyRules(turtle)
-    local success, block = turtle.inspectDown()
+    local success, block = turtle.inspectUp()
     if block.name == nil or block.name == "minecraft:white_wool" then
         while not mine2.selectItem(turtle, "minecraft:black_wool") do end
         turtle.turnRight()
-        turtle.digDown()
-        turtle.placeDown()
+        turtle.digUp()
+        turtle.placeUp()
         turtle.forward()
     end
     if block.name == "minecraft:black_wool" then
         while not mine2.selectItem(turtle, "minecraft:white_wool") do end
         turtle.turnLeft()
-        turtle.digDown()
-        turtle.placeDown()
+        turtle.digUp()
+        turtle.placeUp()
         turtle.forward()
     end
 end
