84829f670bfc4e761e7b04d1c1ff30ceb18d2eca
cchudant <charlesluc.chudant@orange.fr>
2023-09-24 16:29:11 +0200

    fix storage

--- firmware/apis/storage/ui.lua
+++ firmware/apis/storage/ui.lua
@@ -82,7 +82,6 @@ function itemView(storageConnection, makeChild)
     }
 
     local function onTextChange(term, newText)
-        print("change22", block.task)
         search = newText
         if block.task then
             term.removeTask(block.task)
